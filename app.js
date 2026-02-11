a0c6a6219468ef97460e170b705d6a740e4feae5
crymall <reed.gaines@gmail.com>
2026-02-10 19:43:23 -0500

    remove static server logic

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
-import { fileURLToPath } from "url";
 
 import authRouter from "./routes/auth.js";
 import usersRouter from "./routes/users.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -21,8 +16,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/users", usersRouter);
 app.use("/", authRouter);
